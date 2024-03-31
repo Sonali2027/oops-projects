12ae9f3ad85ee2f0d9df50c0aae994eecdfca62e
Sonali2027 <163150966+Sonali2027@users.noreply.github.com>
2024-04-01 01:25:52 +0530

    iteration statement

--- github/assignment1.cpp
+++ github/assignment1.cpp
@@ -1,3 +1,7 @@
+//In the field of programming, Iteration Statements are helpful when we need a specific task in repetition.
+//They are essential as they reduce hours of work to seconds. An iteration is a sequence of instructions that is continually repeated until a certain condition is reached.
+//As per given statement you need to implement the task using iterations.
+//A number is said to be valid if it is divisible by 8. Develop a logic that allows the user to keep entering numbers as long as the input is valid and also displays a count of the valid numbers.
 #include<iostream>
 using namespace std;
 class valid{
